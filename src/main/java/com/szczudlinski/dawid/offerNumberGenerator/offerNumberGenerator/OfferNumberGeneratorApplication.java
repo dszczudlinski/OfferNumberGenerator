bf789d00eab15e8b112897badc7b440397a37c92
Dawid Szczudliński <dszczudlinski@bluesoft.net.pl>
2018-06-23 17:26:33 +0200

    Inicjalny projekt

---
+++ src/main/java/com/szczudlinski/dawid/offerNumberGenerator/offerNumberGenerator/OfferNumberGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.szczudlinski.dawid.offerNumberGenerator.offerNumberGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferNumberGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferNumberGeneratorApplication.class, args);
+	}
+}
